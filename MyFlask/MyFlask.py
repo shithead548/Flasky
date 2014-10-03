b89612a039aa4152d5567b3b6e1a56c2625040a2
Stanley Wu <bleedtodry@hotmail.com>
2014-10-03 14:05:34 +0800

    添加模板

--- MyFlask/MyFlask.py
+++ MyFlask/MyFlask.py
@@ -1,14 +1,18 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
+from flask_script import Manager
 
+app = Flask(__name__)
+#manager = Manager(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html',name=name)
 
 if __name__ == '__main__':
+    #manager.run();
     app.run(debug=True)
