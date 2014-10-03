3b5fc639cb80cb8c7df855fec69d783e9aab2a58
Stanley Wu <bleedtodry@hotmail.com>
2014-10-03 16:20:00 +0800

    添加时区设置

--- MyFlask/MyFlask.py
+++ MyFlask/MyFlask.py
@@ -2,14 +2,17 @@ from flask import Flask
 from flask import render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 #manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time = datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
@@ -22,7 +25,6 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'),500
-
 
 if __name__ == '__main__':
     #manager.run();
