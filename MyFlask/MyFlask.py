18452a6b079ba2211deb3c02bef41b7716d832fe
Stanley Wu <bleedtodry@hotmail.com>
2014-10-03 14:43:21 +0800

    添加bootstrap模板

--- MyFlask/MyFlask.py
+++ MyFlask/MyFlask.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask import render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 #manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
