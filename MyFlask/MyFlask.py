32ca2c1e046c9c298b79b6ce0bb1f55c052a4258
Stanley Wu <bleedtodry@hotmail.com>
2014-10-03 21:58:09 +0800

    增加一个表单

--- MyFlask/MyFlask.py
+++ MyFlask/MyFlask.py
@@ -3,28 +3,44 @@ from flask import render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField
+from wtforms import SubmitField
+from wtforms.validators import Required
+
 
 app = Flask(__name__)
-#manager = Manager(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html', current_time = datetime.utcnow())
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html',name=name)
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'),404
+    return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'),500
+    return render_template('500.html'), 500
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
+
 
 if __name__ == '__main__':
     #manager.run();
